7e670be8e0264ffafebcdda98e1bc6834481d8cf
rileyyy <rarico@pratt.edu>
2026-04-07 15:22:46 -0400

    Figured out manageSeeds kind of

--- PhysicsGame_GameCoding2_Unity/Assets/Scripts/SeedManager.cs
+++ PhysicsGame_GameCoding2_Unity/Assets/Scripts/SeedManager.cs
@@ -4,30 +4,43 @@ using UnityEngine;
 public class SeedManager : MonoBehaviour
 {
     public GameObject seedPrefab;
-    public int amountToSpawn = 3;
+    public int amountToSpawn = 1;
     private int seedCount;
+    //private IEnumerator coroutine;
+
+    private void Start()
+    {
+        //coroutine = Spawn();
+    }
 
     private void Update()
     {
         GameObject[] hello = GameObject.FindGameObjectsWithTag(seedPrefab.tag);
         seedCount = hello.Length;
-        if(seedCount <= 0)
+        if(seedCount <= 3)
         {
-            StartCoroutine(Spawn());
+            SpawnMoreSeeds();
+            //StartCoroutine(coroutine);
         }
     }
 
-    /*private void SpawnMoreSeeds()
+    private void SpawnMoreSeeds()
     {
-        for(int i = 0; i < amountToSpawn; i++)
+        /*for(int i = 0; i < amountToSpawn; i++)
         {
             Instantiate(seedPrefab, this.gameObject.transform);
+            float timer = 5f;
+            while(timer >= 0)
+            {
+                timer -= Time.deltaTime;
+            }
             
-        }
+        }*/
+        Instantiate(seedPrefab, this.gameObject.transform);
         Debug.Log("Spawned new " + seedPrefab.name);
-    }*/
+    }
 
-    IEnumerator Spawn()
+    /*IEnumerator Spawn()
     {
         for (int i = 0; i < amountToSpawn; i++)
         {
@@ -35,5 +48,5 @@ public class SeedManager : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
         }
         Debug.Log("Spawned new " + seedPrefab.name);
-    }
+    }*/
 }
