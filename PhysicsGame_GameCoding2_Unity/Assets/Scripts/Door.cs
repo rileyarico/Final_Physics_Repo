2db932afcbb24aff312d7ff7dbfdbc21c043c8f5
rileyarico <rarico@pratt.edu>
2026-05-13 03:26:33 -0400

    Revert "Fixing"
    This reverts commit 850a73ac6d4a5ef24b0f3856617f28aebce2806c.

--- PhysicsGame_GameCoding2_Unity/Assets/Scripts/Door.cs
+++ PhysicsGame_GameCoding2_Unity/Assets/Scripts/Door.cs
@@ -5,9 +5,18 @@ public class Door : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        this.gameObject.SetActive(false);
     }
 
+    public void DeactivateThis()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void ActivateThis()
+    { 
+        this.gameObject.SetActive(true); 
+    }
     // Update is called once per frame
     void Update()
     {
